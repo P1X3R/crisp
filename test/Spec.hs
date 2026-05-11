5e5ae6132ee2b9f7cf35f647d1ea1e1cb97333ed
P1X3R <p1x3r@proton.me>
2026-05-10 20:38:51 -0500

    test(lexer): setup tests and add test for empty program logic

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Lexer (ParserState (..), Position (..), Token (..), TokenData (..), runTokenizer)
+import Test.Hspec
+import Test.Hspec.Hedgehog
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "runTokenizer" $ do
+        it "property: empty program is just eof" $ do
+            runTokenizer "" `shouldBe` Right [Token TEof (Position 1 1)]
