b49ddc5d8bdbfbe063c3fe8c5ad0b73a0a302437
P1X3R <p1x3r@proton.me>
2026-05-14 15:13:13 -0500

    test(lexer): add a valid expression generator for testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,67 @@ import Lexer (ParserState (..), Position (..), Token (..), TokenData (..), runTo
 import Test.Hspec
 import Test.Hspec.Hedgehog
 
+minInt :: Int
+minInt = -100
+maxInt :: Int
+maxInt = 100
+
+minFloat :: Float
+minFloat = -1.0
+maxFloat :: Float
+maxFloat = 1.0
+
+minStrLen :: Int
+minStrLen = 10
+maxStrLen :: Int
+maxStrLen = 100
+
+minSymbolLen :: Int
+minSymbolLen = 1
+maxSymbolLen :: Int
+maxSymbolLen = 20
+
+minExprLen :: Int
+minExprLen = 0
+maxExprLen :: Int
+maxExprLen = 10
+
+genNumber :: Gen String
+genNumber =
+    let randInt = fmap show $ Gen.int (Range.linear minInt maxInt)
+        randFloat = fmap show $ Gen.float (Range.linearFrac minFloat maxFloat)
+     in Gen.choice [randInt, randFloat]
+
+genBool :: Gen String
+genBool = Gen.element ["#t", "#f"]
+
+genString :: Gen String
+genString = do
+    content <- Gen.string (Range.linear minStrLen maxStrLen) Gen.alphaNum
+    pure $ "\"" <> content <> "\""
+
+genSymbol :: Gen String
+genSymbol = do
+    let specialSymbols = "+-*/><=!?_"
+    first <- Gen.choice [Gen.alpha, Gen.element specialSymbols]
+    rest <-
+        Gen.string (Range.linear (minSymbolLen - 1) (maxSymbolLen - 1)) $
+            Gen.choice [Gen.alphaNum, Gen.element specialSymbols]
+    pure $ first : rest
+
+genAtom :: Gen String
+genAtom = Gen.choice [genNumber, genBool, genString, genSymbol]
+
+genExpr :: Gen String
+genExpr =
+    Gen.recursive
+        Gen.choice
+        [ genAtom
+        ]
+        [ fmap (\s -> "(" <> unwords s <> ")") (Gen.list (Range.linear minExprLen maxExprLen) genExpr)
+        , fmap ("'" <>) genExpr
+        ]
+
 main :: IO ()
 main = hspec $ do
     describe "runTokenizer" $ do
@@ -12,10 +73,8 @@ main = hspec $ do
             runTokenizer "" `shouldBe` Right [Token TEof (Position 1 1)]
 
         it "property: tokens' position always strictly advance" $ hedgehog $ do
-            -- Generate 1 to 100 words, each 1 to 10 characters long
-            xsList <- forAll $ Gen.list (Range.linear 1 100) $ Gen.string (Range.linear 1 10) Gen.alpha
+            input <- forAll $ genExpr
 
-            let input = unwords xsList
             case runTokenizer input of
                 Left err -> do
                     annotateShow err
